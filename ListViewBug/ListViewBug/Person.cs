312ed49c4d0f844e1bb931818a19248ffc4385b9
אלי ברק <elybarak7589@gmail.com>
2018-10-25 13:45:17 +0300

    Project

---
+++ ListViewBug/ListViewBug/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LiteDB;
+using Xamarin.Forms;
+
+namespace ListViewBug
+{
+    class Person : BindableObject
+    {
+        public IEnumerable<BsonDocument> MyProperty { get; set; }
+
+        public Person()
+        {
+            MyProperty = new BsonDocument[] {
+                new BsonDocument{
+                    { "first", "something" },
+                    { "second", "SecondSomething" },
+                    { "third", "ThirdSomething" }
+                },
+                 new BsonDocument{
+                    { "first", "something" },
+                    { "second", "SecondSomething" },
+                    { "third", "ThirdSomething" }
+                },
+                new BsonDocument{
+                    { "first", "something" },
+                    { "second", "SecondSomething" },
+                    { "third", "ThirdSomething" }
+                }
+            };
+        }
+    }
+}
